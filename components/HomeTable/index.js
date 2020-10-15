3f30fcd4bd275784df00b6026ac1aa3bc89785b4
Nemanja Manot <manotnemanja@gmail.com>
2020-10-16 00:50:55 +0200

    search functionality

--- components/HomeTable/index.js
+++ components/HomeTable/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -6,10 +7,13 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import HomeTableHeader from './HomeTableHeader';
-
 // temporary mocked data
 import { data } from '../../dataMock';
+// Styles
+import styles from '../../styles/Home.module.css';
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -37,11 +41,26 @@ function stableSort(array, comparator) {
   return stabilizedThis.map((el) => el[0]);
 }
 
+let debounce;
+
 export default function EnhancedTable() {
   const [order, setOrder] = useState('desc');
   const [orderBy, setOrderBy] = useState('volume');
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [searchedResult, setSearchedResult] = useState(data);
+
+  useEffect(() => {
+    setSearchedResult(data);
+  }, [data]);
+
+  useEffect(() => {
+    return () => {
+      if (debounce) {
+        clearTimeout(debounce);
+      }
+    };
+  }, []);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -58,8 +77,18 @@ export default function EnhancedTable() {
     setPage(0);
   };
 
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    if (debounce) {
+      clearTimeout(debounce);
+    }
+    debounce = setTimeout(() => {
+      setSearchedResult(data.filter((currency) => currency.symbol.toLowerCase().includes(query.toLowerCase())));
+    }, 500);
+  };
+
   const renderTableBody = () => {
-    return stableSort(data, getComparator(order, orderBy))
+    return stableSort(searchedResult, getComparator(order, orderBy))
       .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
       .map((row, index) => {
         const labelId = `enhanced-table-checkbox-${index}`;
@@ -78,22 +107,33 @@ export default function EnhancedTable() {
   };
 
   return (
-    <Paper>
-      <TableContainer>
-        <Table aria-labelledby="tableTitle" aria-label="enhanced table">
-          <HomeTableHeader order={order} orderBy={orderBy} onRequestSort={handleRequestSort} />
-          <TableBody>{renderTableBody()}</TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 20]}
-        component="div"
-        count={data.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <div>
+      <div className={styles.tableHeader}>
+        <Link href={`/alerts/`}>
+          <Button variant="contained" color="primary">
+            Alerts
+          </Button>
+        </Link>
+        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" onChange={handleSearch} />
+      </div>
+
+      <Paper>
+        <TableContainer>
+          <Table aria-labelledby="tableTitle" aria-label="enhanced table">
+            <HomeTableHeader order={order} orderBy={orderBy} onRequestSort={handleRequestSort} />
+            <TableBody>{renderTableBody()}</TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 20]}
+          component="div"
+          count={searchedResult.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import HomeTable from '../components/HomeTable';
 // Styles
 import styles from '../styles/Home.module.css';
@@ -14,15 +11,6 @@ export default function Home() {
         <title>Crypto Currency Market</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className={styles.tableHeader}>
-        <Link href={`/alerts/`}>
-          <Button variant="contained" color="primary">
-            Alerts
-          </Button>
-        </Link>
-        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-      </div>
 
       <HomeTable />
     </div>
