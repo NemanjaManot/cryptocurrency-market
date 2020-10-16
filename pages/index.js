0289e799d4c17cd88a84e0df1ea5774790aa3882
Nemanja Manot <manotnemanja@gmail.com>
2020-10-16 14:38:58 +0200

    started with logic for fetching every 5 minutes

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,46 @@ import { data } from '../dataMock';
 // Styles
 import styles from '../styles/Home.module.css';
 
+// 5 minutes in milliseconds
+const timeToReFetchInMs = 300000;
+
 export default function Home() {
   const [list, setList] = useState([]);
+  const [currentTime, setCurrentTime] = useState(null);
+  const [lastGetTime, setLastGetTime] = useState(null);
+
+  const setCurrentTimeInMsToStorage = () => {
+    const date = new Date();
+    const msTime = date.getTime();
+    setItemToLocalStorage('currentTimeMs', msTime);
+  };
+
+  const setLastGetTimeInMsToStorage = () => {
+    const date = new Date();
+    const msTime = date.getTime();
+    setItemToLocalStorage('lastGetTimeMs', msTime);
+  };
 
   useEffect(() => {
-    setItemToLocalStorage('currenciesList', data);
+    setCurrentTimeInMsToStorage();
+    if (!lastGetTime) {
+      // this should be triggered only for first time (because lastGetTime still doesn't exist in local storage)
+      setItemToLocalStorage('currenciesList', data);
+      console.log('only for first time');
+      return;
+    }
+    if (currentTime - lastGetTime > timeToReFetchInMs) {
+      // this should be triggered only if time from previous fetch is less then 5 minutes
+      setItemToLocalStorage('currenciesList', data);
+      console.log('after 5 minutes');
+    }
   }, []);
 
   useEffect(() => {
+    // get currentTimeMs from local storage
+    let currentTimeMs = getItemFromLocalStorage('currentTimeMs');
+    setCurrentTime(currentTimeMs);
+    // get listOfCurrencies from local storage
     let listOfCurrencies = getItemFromLocalStorage('currenciesList');
     setList(listOfCurrencies);
   }, []);
